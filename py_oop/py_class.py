f502d56d049be26426b67ae1748b8a1d7e4362bc
Administrator <heutwo@naver.com>
2018-11-17 16:23:58 +0900

    first commit

---
+++ py_oop/py_class.py
@@ -0,0 +1,13 @@
+# ******************
+# -- 클래스 선언
+# ******************
+class MyClass:
+    pass
+
+# Member m = new Member() 자바방식
+a = MyClass() # 파이썬
+b = MyClass()
+print(a)
+print(b)
+
+
---
+++ tf_basic/tf_test.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+a = tf.placeholder("float")
+b = tf.placeholder("float")
+c = tf.multiply(a,b)
+sess = tf.Session()
+print(sess.run(c, feed_dict={a: 3, b:2}))
